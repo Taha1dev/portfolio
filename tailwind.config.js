263d6422f5efc1677a39a18cb55cc40712ce1ea2
Taha <Taha1Dev.com>
2023-09-28 21:55:36 -0700

    tailwind utility customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,8 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: '#009688',
+        primary: 'var(--textColor)',
         mainBg: '#31333b',
-        skill: '#181c3c',
       },
     },
   },
