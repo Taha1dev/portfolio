81d810f2445261b327aff381bbbefd44fc7d580f
Taha <Taha1Dev.com>
2023-10-02 17:54:02 -0700

    improve meta

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,57 @@ import { Montserrat } from 'next/font/google';
 const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['100', '300', '400', '700'],
+  display: 'swap',
 });
 import { ThemeProvider } from '@/context/ContextProvider';
+import { useEffect } from 'react';
+import Head from 'next/head';
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((registration) => {
+            console.log(
+              'Service Worker registered with scope:',
+              registration.scope
+            );
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
+    }
+  }, []);
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/imgs/logo.png"></link>
+        <meta name="theme-color" content="#051823" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="I'm a Frontend Developer with 2 Years Experience of Self Learning and Projects Experience,"
+        />
+        <meta name="copyright" content="Taha Al-Mulla" />
+        <meta
+          name="keywords"
+          content="Taha, Al-Mulla, Mulla, frontend, Front-end, React, Reactjs, Next, Next.js, Web, webdeveloper, freelancer, uiux, designer, bussiness, web3, PWA, performance, SEO, Optimization"
+        />
+        <meta name="author" content="Taha Al-Mulla" />
+        <meta property="og:title" content="Taha Mulla Frontend Developer" />
+        <meta
+          property="og:description"
+          content="Hello My Name is Taha aka Dr.Web i'm a Software Engineer specalize in Building web applicatoins"
+        />
+        <meta property="og:url" content="https://tahamulla.vercel.app/" />
+      </Head>
       <style jsx global>{`
         html {
           font-family: ${montserrat.style.fontFamily};
--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,8 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
-        <meta name="theme-color" content="#051823" />
-      </Head>
+      <Head />
+
       <body>
         <Main />
         <NextScript />
