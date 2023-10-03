f2234c49b664493f7072497aa0398261b3fa2a8c
Taha <Taha1Dev.com>
2023-10-03 08:34:57 -0700

    improve animation performance

--- components/Hero/Animation.jsx
+++ components/Hero/Animation.jsx
@@ -1,31 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 
 export const Animation = () => {
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
-
-  const checkScreenSize = useCallback(() => {
-    setIsLargeScreen(window.innerWidth >= 1500);
-  }, []);
-
-  useEffect(() => {
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
-    return () => {
-      window.removeEventListener('resize', checkScreenSize);
-    };
-  }, [checkScreenSize]);
-
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async () => {}, []);
 
-  if (!isLargeScreen) {
-    return null; 
-  }
+  useEffect(() => {
+    particlesLoaded();
+  }, [particlesLoaded]);
 
   return (
     <Particles
