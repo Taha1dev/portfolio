163f676e7f31162d676c89365831ed4d01c6dd99
Taha <Taha1Dev.com>
2023-10-02 17:53:44 -0700

    customize taildind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
         primary: 'var(--textColor)',
         mainBg: 'var(--mainColor)',
-        secondary: 'var(--secondColor)'
+        secondary: 'var(--secondColor)',
       },
     },
   },
